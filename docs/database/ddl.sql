64e029b45ff7be1bb32581e7e62832b48f4c8725
Ivan Meireles de Abreu <132685712+ivanmeirelesdeabreu@users.noreply.github.com>
2026-05-11 13:35:02 -0300

    Update coligacao table primary key and foreign keys

--- docs/database/ddl.sql
+++ docs/database/ddl.sql
@@ -91,14 +91,13 @@ CREATE TABLE federacao (
 CREATE TABLE coligacao (
     sq_coligacao BIGINT,
     cd_eleicao bigint,
-    sg_uf CHAR(2),
-    sg_ue VARCHAR(10),
+    cd_municipio bigint,
     nm_coligacao VARCHAR(150),
     ds_composicao_coligacao TEXT,
-    PRIMARY KEY (sq_coligacao, cd_eleicao, sg_uf, sg_ue),
+    PRIMARY KEY (sq_coligacao, cd_eleicao, cd_municipio),
 
     FOREIGN KEY (cd_eleicao) REFERENCES eleicao(cd_eleicao),
-    FOREIGN KEY (sg_uf, sg_ue) REFERENCES municipio(sg_uf, sg_ue)
+    FOREIGN KEY (cd_municipio) REFERENCES municipio(cd_municipio)
 );
 
 CREATE TABLE genero (
@@ -142,6 +141,7 @@ CREATE TABLE candidato (
     
     cd_eleicao bigint,
     cd_municipio bigint,
+
     cd_cargo bigint,
     nr_partido bigint,
     nr_federacao bigint,
@@ -172,7 +172,7 @@ CREATE TABLE candidato (
     FOREIGN KEY (cd_cargo) REFERENCES cargo(cd_cargo),
     FOREIGN KEY (nr_partido) REFERENCES partido(nr_partido),
     FOREIGN KEY (nr_federacao) REFERENCES federacao(nr_federacao),
-    FOREIGN KEY (sq_coligacao, cd_eleicao, sg_uf, sg_ue) REFERENCES coligacao(sq_coligacao, cd_eleicao, sg_uf, sg_ue),
+    FOREIGN KEY (sq_coligacao, cd_eleicao, cd_municipio) REFERENCES coligacao(sq_coligacao, cd_eleicao, cd_municipio),
     FOREIGN KEY (cd_genero) REFERENCES genero(cd_genero),
     FOREIGN KEY (cd_grau_instrucao) REFERENCES grau_instrucao(cd_grau_instrucao),
     FOREIGN KEY (cd_estado_civil) REFERENCES estado_civil(cd_estado_civil),
@@ -181,4 +181,3 @@ CREATE TABLE candidato (
     FOREIGN KEY (cd_situacao_candidatura) REFERENCES situacao_candidatura(cd_situacao_candidatura),
     FOREIGN KEY (cd_sit_tot_turno) REFERENCES situacao_turno(cd_sit_tot_turno)
 );
-
